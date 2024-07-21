de7e48d83f48f6ae5e94d861f7b46d44c78ea7d2
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-07-21 11:55:20 +0900

    記事の作成日でフィルタリング #4

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,18 +1,8 @@
-/* eslint-disable no-irregular-whitespace */
 import { useState } from "react";
 
-// キーワードの正規化関数
-const normalizeString = (keywords: string) => {
-  return keywords
-    .replace(/，/g, " ")
-    .replace(/,/g, " ")
-    .replace(/、/g, " ")
-    .replace(/　/g, " ")
-    .replace(/\s+/g, " ")
-    .trim();
-};
+import { convertCreatedAtRange, normalizeString } from "../lib/utils";
+import { createdAtRange } from "../types";
 
-// 検索フォームコンポーネント
 export function SearchForm({
   getItemsByQuery,
 }: {
@@ -21,54 +11,85 @@ export function SearchForm({
   const [keyword, setKeyword] = useState("");
   const [tags, setTags] = useState("");
   const [stocksCount, setStocksCount] = useState(100);
+  const [createdAtRange, setCreatedAtRange] = useState<createdAtRange>("1y");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     const normalizedKeyword = normalizeString(keyword);
     const normalizedTags = normalizeString(tags);
+    const createdAt = convertCreatedAtRange(createdAtRange);
+
     const query = `${normalizedKeyword} ${normalizedTags
       .split(" ")
       .map((tag) => `tag:${tag}`)
-      .join(" ")} stocks:>${stocksCount}`;
+      .join(" ")} stocks:>${stocksCount} created:>${createdAt}`;
+
     getItemsByQuery(query);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-4">
+    <form onSubmit={handleSubmit} className="my-4">
+      <label htmlFor="keyword" className="block mb-1 font-semibold">
+        Keyword (space-separated)
+      </label>
       <input
         type="text"
         value={keyword}
+        id="keyword"
         onChange={(e) => setKeyword(e.target.value)}
-        placeholder="Keyword (space-separated)"
+        placeholder="JavaScript React ..."
         className="p-2 border border-gray-300 rounded mb-2 w-full"
       />
+      <label htmlFor="tags" className="block mb-1 font-semibold">
+        Tags (space-separated)
+      </label>
       <input
         type="text"
+        id="tags"
         value={tags}
         onChange={(e) => setTags(e.target.value)}
-        placeholder="Tags (space-separated)"
+        placeholder="to perform a more refined search"
         className="p-2 border border-gray-300 rounded mb-2 w-full"
       />
-      <div className="mb-2">
-        <label htmlFor="stocksCount" className="block mb-1 font-semibold">
-          Stocks Count
-        </label>
-        <select
-          name="stocksCount"
-          value={stocksCount}
-          onChange={(e) => setStocksCount(Number(e.target.value))}
-          id="stocksCount"
-          className="p-2 border border-gray-300 rounded w-full"
-        >
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="10">10</option>
-          <option value="50">50</option>
-          <option value="100">100</option>
-          <option value="500">500</option>
-          <option value="1000">1000</option>
-        </select>
-      </div>
+
+      <label htmlFor="stocksCount" className="block mb-1 font-semibold">
+        Stocks Count (more than)
+      </label>
+      <select
+        name="stocksCount"
+        value={stocksCount}
+        onChange={(e) => setStocksCount(Number(e.target.value))}
+        id="stocksCount"
+        className="p-2 border border-gray-300 rounded mb-2 w-full"
+      >
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="10">10</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+        <option value="500">500</option>
+        <option value="1000">1000</option>
+      </select>
+
+      <label htmlFor="createdAtRange" className="block mb-1 font-semibold">
+        Range of creation date
+      </label>
+      <select
+        name="createdAtRange"
+        id="createdAtRange"
+        value={createdAtRange}
+        onChange={(e) => setCreatedAtRange(e.target.value as createdAtRange)}
+        className="p-2 border border-gray-300 rounded mb-4 w-full"
+      >
+        <option value="1w">Week</option>
+        <option value="1m">Month</option>
+        <option value="1y">Year</option>
+        <option value="5y">5 Year</option>
+        <option value="10y">10 Year</option>
+        <option value="all">All time</option>
+      </select>
+
       <button
         type="submit"
         className="p-2 bg-blue-600 text-white rounded w-full"
---
+++ src/lib/utils.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-irregular-whitespace */
+import { createdAtRange } from "../types";
+
+// 文字列を正規化する関数
+export const normalizeString = (keywords: string) => {
+  return keywords
+    .replace(/，/g, " ")
+    .replace(/,/g, " ")
+    .replace(/、/g, " ")
+    .replace(/　/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+};
+
+// createdAtRangeを(year-month-date)に変換する関数
+export const convertCreatedAtRange = (createdAtRange: createdAtRange) => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+
+  switch (createdAtRange) {
+    case "1w":
+      return `${year}-${month}-${date - 7}`;
+    case "1m":
+      return `${year}-${month - 1}-${date}`;
+    case "1y":
+      return `${year - 1}-${month}-${date}`;
+    case "5y":
+      return `${year - 5}-${month}-${date}`;
+    case "10y":
+      return `${year - 10}-${month}-${date}`;
+    case "all":
+      return "2011-01-01";
+  }
+};
