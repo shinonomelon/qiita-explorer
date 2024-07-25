79e792f586b6e793ffc015ec438a0daffa7f13b2
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-07-25 23:41:53 +0900

    mを大文字にして分と見分ける

--- src/types.ts
+++ src/types.ts
@@ -17,4 +17,4 @@ export type QiitaItem = {
   user: QiitaUser;
 };
 
-export type CreatedAtRange = "1w" | "1m" | "1y" | "5y" | "10y" | "all";
+export type CreatedAtRange = "7d" | "1M" | "1y" | "5y" | "10y" | "all";
